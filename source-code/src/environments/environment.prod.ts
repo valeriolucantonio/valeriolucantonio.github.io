291beddf51799b89b9dab7db20b5e0ee57ec2969
valerio lucantonio <valerio.lucantonio@gmail.com>
2022-02-26 16:08:18 +0100

    small bug-fix

--- source-code/src/environments/environment.prod.ts
+++ source-code/src/environments/environment.prod.ts
@@ -9,8 +9,4 @@ import { env } from './.env';
 export const environment = {
   production: true,
   version: env['npm_package_version'],
-  serverUrl: 'https://api.chucknorris.io',
-  defaultLanguage: 'en-US',
-  supportedLanguages: ['en-US', 'fr-FR'],
 };
-
